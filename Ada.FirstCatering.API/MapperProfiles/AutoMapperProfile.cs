28a2765c1b534e2b2d727a22bbefdadadc3ecfb2
Alex Redding <Alexredding180@gmail.com>
2022-03-11 01:56:02 +0000

    Card session service + Automapper

---
+++ Ada.FirstCatering.API/MapperProfiles/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ada.FirstCatering.API.Models.Entities;
+using Ada.FirstCatering.API.Models.Requests;
+using AutoMapper;
+
+namespace Ada.FirstCatering.API.MapperProfiles;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<CreateEmployeeModel, Employee>();
+    }
+}
---
+++ Ada.FirstCatering.API/Services/CardSessionService.cs
@@ -0,0 +1,40 @@
+﻿using Ada.FirstCatering.API.Models.Responses;
+
+namespace Ada.FirstCatering.API.Services;
+
+public class CardSessionService
+{
+    private Dictionary<string, CardSession> CardSessions { get; set; } = new();
+
+    public CardSession? GetCardSession(string cardId)
+    {
+        var session = CardSessions.TryGetValue(cardId, out var cardSession) ? cardSession : null;
+        if (session != null)
+        {
+            if(session.Expiration < DateTime.UtcNow)
+            {
+                DeleteCardSession(cardId);
+                return null;
+            }
+        }
+
+        return session;
+    }
+    
+    public CardSession CreateCardSession(string cardId)
+    {
+        var cardSession = new CardSession(cardId);
+        CardSessions.Add(cardSession.CardId, cardSession);
+        return cardSession;
+    }
+    
+    public void DeleteCardSession(string cardId)
+    {
+        CardSessions.Remove(cardId);
+    }
+
+    public void UpdateCardSessionExpiration(string cardId)
+    {
+        GetCardSession(cardId)?.UpdateExpiration();
+    }
+}
